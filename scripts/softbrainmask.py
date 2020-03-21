e1c223e25640f561fb79c70d831dba7e7b30cea6
oesteban <code@oscaresteban.es>
2020-03-20 18:37:13 -0700

    update soft brainmask script

--- scripts/softbrainmask.py
+++ scripts/softbrainmask.py
@@ -1,10 +1,8 @@
 from templateflow import api
-from scikit.ndimage import gaussian_filter
 from scipy.ndimage import gaussian_filter
 import nibabel as nb
 import numpy as np
 from skimage.morphology import ball
-from scikit.ndimage import binary_erosion
 from scipy.ndimage import morphology as m
 
 path = api.get('MNI152NLin2009cAsym', desc='brain', resolution=1, suffix='mask')
@@ -14,6 +12,8 @@ dilate1 = m.binary_dilation(data == 1, ball(1))
 dilate2 = m.binary_dilation(dilate1, ball(1))
 soft = 0.95 * dilate2
 soft[dilate1] = 1.0
-gauss = gaussian_filter(soft, 1)
-nb.Nifti1Image(gauss, im.affine, im.header).to_filename('gauss.nii.gz')
+gauss = gaussian_filter(soft.astype('float32'), 1)
+header = im.header.copy()
+header.set_data_dtype('float32')
+nb.Nifti1Image(gauss.astype('float32'), im.affine, header).to_filename('tpl-MNI152NLin2009cAsym_res-01_desc-brain_probseg.nii.gz')
 
